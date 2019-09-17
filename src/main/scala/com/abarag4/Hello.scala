f31f16c498d02ddeb302674eb870cc20862ee039
Amedeo Baragiola <ingamedeo@gmail.com>
2019-09-17 15:44:37 -0500

    Add jars

--- src/main/scala/com/abarag4/Hello.scala
+++ src/main/scala/com/abarag4/Hello.scala
@@ -1,7 +1,27 @@
 package com.abarag4
 
+import java.util.Calendar
+
 import com.typesafe.config.ConfigFactory
 import org.slf4j.{Logger, LoggerFactory}
+import org.cloudbus.cloudsim.Cloudlet
+import org.cloudbus.cloudsim.CloudletSchedulerTimeShared
+import org.cloudbus.cloudsim.Datacenter
+import org.cloudbus.cloudsim.DatacenterBroker
+import org.cloudbus.cloudsim.DatacenterCharacteristics
+import org.cloudbus.cloudsim.Host
+import org.cloudbus.cloudsim.Log
+import org.cloudbus.cloudsim.Pe
+import org.cloudbus.cloudsim.Storage
+import org.cloudbus.cloudsim.UtilizationModel
+import org.cloudbus.cloudsim.UtilizationModelFull
+import org.cloudbus.cloudsim.Vm
+import org.cloudbus.cloudsim.VmAllocationPolicySimple
+import org.cloudbus.cloudsim.VmSchedulerTimeShared
+import org.cloudbus.cloudsim.core.CloudSim
+import org.cloudbus.cloudsim.provisioners.BwProvisionerSimple
+import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple
+import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple
 
 object Hello {
 
@@ -19,5 +39,12 @@ object Hello {
     LOG.info("I am fine.")
     LOG.warn("I love programming.")
     LOG.error("I am programming.")
+
+    val num_user = 1 // number of cloud users
+    val calendar = Calendar.getInstance // Calendar whose fields have been initialized with the current date and time.
+    val trace_flag = false // trace events
+
+    CloudSim.init(num_user, calendar, trace_flag)
+
   }
 }
